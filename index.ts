683f65e9c059318df65c377da4ac65d2b96e8ad3
miamollie <mia.deburca@99designs.com>
2021-06-10 12:51:38 +1000

    Add comments to stack

--- index.ts
+++ index.ts
@@ -11,18 +11,21 @@ export class SaleNotifierStack extends cdk.Stack {
   constructor(app: cdk.App, id: string) {
     super(app, id);
 
+    //Create detection lambda
     const saleDetectionFn = new lambda.Function(this, "SaleDetectionHandler", {
       code: lambda.Code.fromAsset("lambda"),
       handler: "sale-detector.handler",
       runtime: lambda.Runtime.NODEJS_10_X,
     });
 
+    //Create notification lambda
     const saleNotifierFn = new lambda.Function(this, "SaleNotifierHandler", {
       code: lambda.Code.fromAsset("lambda"),
       handler: "sale-notifier.handler",
       runtime: lambda.Runtime.NODEJS_10_X,
     });
 
+    //Grant notifier lambda permission to send email
     const iAmStatement = new iam.PolicyStatement({
       effect: Effect.ALLOW,
     });
@@ -30,17 +33,21 @@ export class SaleNotifierStack extends cdk.Stack {
     iAmStatement.addResources("*");
     saleNotifierFn.addToRolePolicy(iAmStatement);
 
+    //Create pub/sub notification topic
     const topic = new sns.Topic(this, "Sale", {
       displayName: "New sale topic",
       topicName: "saleTopic",
     });
 
+    //Subscribe the notification lambda to the pub/sub rule
     topic.addSubscription(new subs.LambdaSubscription(saleNotifierFn));
 
+    //Create scheduled event
     const rule = new events.Rule(this, "Rule", {
       schedule: events.Schedule.rate(cdk.Duration.days(1)),
     });
 
+    //Add the sale detection lambda to the scheduled event
     rule.addTarget(new targets.LambdaFunction(saleDetectionFn));
   }
 }
